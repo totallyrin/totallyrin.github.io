c6c1fb8ceffb859aa7d30a43949fba1cc5e5c0ab
Lucy W <lucy.woloszczuk@gmail.com>
2024-04-23 15:10:31 -0400

    some updates or something

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,35 +3,123 @@ import {
   Flex,
   Heading,
   HStack,
+  IconButton,
   Spacer,
-  Text,
-  useTheme
+  useMediaQuery,
+  useTheme,
 } from "@chakra-ui/react";
-import {sunflower} from "../utils/utils";
+import { bg, sunflower } from "../utils/utils";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export default function Navbar() {
   const theme = useTheme();
+  const [onMobile] = useMediaQuery("(min-width: 800px)", {
+    ssr: true,
+    fallback: false, // return false on the server, and re-evaluate on the client side
+  });
 
   return (
-    <Flex sx={{
-      // bg: bg,
-      color: sunflower,
-      p: 10
-    }}>
-      <Button variant="ghost" colorScheme="sunflower"
-              size="lg">totallyrin</Button>
-      <Spacer/>
+    <Flex
+      sx={{
+        // bg: bg,
+        color: sunflower,
+        p: 10,
+      }}
+    >
+      <Button
+        variant="ghost"
+        colorScheme="sunflower"
+        size="lg"
+        sx={{
+          _hover: {
+            bg: sunflower,
+            color: bg,
+          },
+        }}
+      >
+        totallyrin
+      </Button>
+      <Spacer />
       <Heading></Heading>
-      <Spacer/>
-      <HStack>
-        <Button variant="ghost" colorScheme="sunflower"><Text
-          as="b">01.</Text>About</Button>
-        <Button variant="ghost" colorScheme="sunflower">02. Projects</Button>
-        <Button variant="ghost" colorScheme="sunflower">03. Experience</Button>
-        <Button variant="ghost" colorScheme="sunflower">04. Contact</Button>
-        <Button variant="outline" colorScheme="sunflower"
-                sx={{ml: 1}}>Resume</Button>
-      </HStack>
+      <Spacer />
+      {onMobile ? (
+        <HStack>
+          <Button
+            variant="ghost"
+            colorScheme="sunflower"
+            sx={{
+              _hover: {
+                bg: sunflower,
+                color: bg,
+              },
+            }}
+          >
+            01. About
+          </Button>
+          <Button
+            variant="ghost"
+            colorScheme="sunflower"
+            sx={{
+              _hover: {
+                bg: sunflower,
+                color: bg,
+              },
+            }}
+          >
+            02. Projects
+          </Button>
+          <Button
+            variant="ghost"
+            colorScheme="sunflower"
+            sx={{
+              _hover: {
+                bg: sunflower,
+                color: bg,
+              },
+            }}
+          >
+            03. Experience
+          </Button>
+          <Button
+            variant="ghost"
+            colorScheme="sunflower"
+            sx={{
+              _hover: {
+                bg: sunflower,
+                color: bg,
+              },
+            }}
+          >
+            04. Contact
+          </Button>
+          <Button
+            variant="outline"
+            colorScheme="sunflower"
+            sx={{
+              ml: 1,
+              _hover: {
+                bg: sunflower,
+                color: bg,
+              },
+            }}
+          >
+            Resume
+          </Button>
+        </HStack>
+      ) : (
+        <IconButton
+          variant="ghost"
+          colorScheme="sunflower"
+          icon={<HamburgerIcon />}
+          aria-label="Open Menu"
+          // sx={{
+          //   _hover: {
+          //     bg: sunflower,
+          //     color: bg,
+          //   },
+          // }}
+        />
+      )}
     </Flex>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,51 @@
 "use client";
 
 import Navbar from "../../components/Navbar";
-import {Box, Button, Heading, HStack, Stack, Text} from "@chakra-ui/react";
-import {bg, sunflower} from "../../utils/utils";
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Spacer,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { bg, sunflower } from "../../utils/utils";
 
 export default function Home() {
   return (
-    <Box sx={{bg: bg}}>
-      <Navbar/>
-      <Box sx={{
-        px: "25%",
-        color: sunflower,
-      }}>
-        <Stack
-          direction="column"
-          spacing={2}
-          align="left"
-        >
-          <Text>{"Hi, I'm"}</Text>
-          <HStack><Heading>Lucy!</Heading><Text>{"she / her"}</Text></HStack>
-          <Heading>{"And I really love programming."}</Heading>
+    <Box sx={{ bg: bg }}>
+      <Navbar />
+      <Box
+        sx={{
+          px: { base: "10%", sm: "15%", md: "20%", lg: "25%" },
+          color: sunflower,
+        }}
+      >
+        <Stack direction="column" spacing={2} align="left">
+          {/*<Text></Text>*/}
+          <HStack>
+            <Heading>Hi, I'm Lucy.</Heading>
+            <Text pt={1.5}>[she / her]</Text>
+          </HStack>
+          <Heading size="md">And I really love programming.</Heading>
+          <Spacer />
           <Text>
-            {"I'm a web developer based in Canada. Currently, I'm focusing" +
-              " on honing my frontend skills as well as exploring new and exciting projects."}
+            I'm a web developer based in Ontario, Canada.
+            <Spacer />
+            Currently, I'm focusing on honing my frontend skills as well as
+            exploring new and exciting projects.
           </Text>
-          <Button variant="outline" colorScheme="sunflower">{"Check out some" +
-            " of my projects!"}</Button>
+          <Spacer />
+          <Button
+            variant="outline"
+            colorScheme="sunflower"
+            sx={{
+              _hover: { bg: sunflower, color: bg },
+            }}
+          >
+            Check out some of my projects!
+          </Button>
         </Stack>
       </Box>
     </Box>
