4c3f05db121aa812df85ecd6d3a219dcad3b394c
totallyrin <lucy.woloszczuk@gmail.com>
2025-05-29 22:40:14 -0400

    completed navbar and homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,20 +13,23 @@ import {
   Transition,
 } from "@mantine/core";
 import { pinkie, sunflower } from "@/utils/utils";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useRouter } from "next/navigation";
+import { useEventListener } from "@mantine/hooks";
 
 export default function Page() {
   const router = useRouter();
   const [line, setLine] = useState(0);
   const pause = 1000;
+  const increment = useCallback(() => setLine(() => 100), []);
+  const ref = useEventListener("click", increment);
 
   return (
-    <Stack gap="xs">
+    <Stack gap="xs" ref={ref} px="lg">
       <Center>
         <TextAnimate.Typewriter
           value={"HELLO, WORLD!"}
-          speed={0.05}
+          speed={line == 100 ? 0 : 0.05}
           animate={line >= 0}
           loop={false}
           c={sunflower}
@@ -42,7 +45,7 @@ export default function Page() {
         <Group gap="lg">
           <TextAnimate.Typewriter
             value={"I'm"}
-            speed={0.05}
+            speed={line == 100 ? 0 : 0.05}
             animate={line >= 1}
             loop={false}
             // c={sunflower}
@@ -55,7 +58,7 @@ export default function Page() {
           <Group gap={0}>
             <TextAnimate.Typewriter
               value={"Rin"}
-              speed={0.05}
+              speed={line == 100 ? 0 : 0.05}
               animate={line >= 2}
               loop={false}
               c={pinkie}
@@ -67,7 +70,7 @@ export default function Page() {
             />
             <TextAnimate.Typewriter
               value={"."}
-              speed={0.05}
+              speed={line == 100 ? 0 : 0.05}
               animate={line >= 3}
               loop={false}
               // c={sunflower}
@@ -84,7 +87,7 @@ export default function Page() {
         <TextAnimate.Typewriter
           value={"And I really love programming."}
           c="dimmed"
-          speed={0.05}
+          speed={line == 100 ? 0 : 0.05}
           animate={line >= 4}
           loop={false}
           // c={sunflower}
@@ -99,7 +102,7 @@ export default function Page() {
       <Transition
         mounted={line >= 5}
         transition="fade-down"
-        duration={1000}
+        duration={line == 100 ? 0 : 500}
         timingFunction="ease"
         onEntered={() => setTimeout(() => setLine(line + 1), pause)}
       >
@@ -109,25 +112,44 @@ export default function Page() {
           </Text>
         )}
       </Transition>
+      <Space h="xs" />
       <Transition
         mounted={line >= 6}
-        transition="fade"
-        duration={1000}
+        transition="slide-left"
+        duration={line == 100 ? 0 : 500}
         timingFunction="ease"
-        onEntered={() => setTimeout(() => setLine(line + 1), pause)}
+        onEntered={() => setTimeout(() => setLine(line + 1), 2 * pause)}
       >
         {(styles) => (
           <Text style={styles}>
             I specialise in building modern, responsive web applications using
-            technologies like React, Next.js, Node.js, and MongoDB, with
-            experience in both frontend and backend development.
+            technologies like React, Next.js, and Node.js, with experience in
+            both frontend and backend development.
           </Text>
         )}
       </Transition>
+      <Space h="xs" />
       <Transition
         mounted={line >= 7}
+        transition="slide-left"
+        duration={line == 100 ? 0 : 500}
+        timingFunction="ease"
+        onEntered={() => setTimeout(() => setLine(line + 1), 2 * pause)}
+      >
+        {(styles) => (
+          <Text style={styles}>
+            Whether I&apos;m building components, debugging layout quirks, or
+            integrating third-party APIs, I care about clean, maintainable code,
+            responsive design, and creating seamless user experiences across
+            devices.
+          </Text>
+        )}
+      </Transition>
+      <Space h="xs" />
+      <Transition
+        mounted={line >= 8}
         transition="pop"
-        duration={500}
+        duration={line == 100 ? 0 : 500}
         timingFunction="ease"
       >
         {(styles) => (
