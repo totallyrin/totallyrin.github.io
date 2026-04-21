23b4562faa8e3f859758e0449fa1e6a22a63c1e5
totallyrin <lucy.woloszczuk@gmail.com>
2026-04-21 12:46:51 -0400

    fix layout

--- src/app/resume/Experience.tsx
+++ src/app/resume/Experience.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Container, Stack, Transition } from "@mantine/core";
+import { Stack, Transition } from "@mantine/core";
 import Education from "@/app/resume/Education";
 import Work from "@/app/resume/Work";
 import Skills from "@/app/resume/Skills";
@@ -20,13 +20,11 @@ export default function Experience() {
       timingFunction="ease"
     >
       {(styles) => (
-        <Container mx={0} px={0}>
-          <Stack style={styles}>
-            <Skills />
-            <Work />
-            <Education />
-          </Stack>
-        </Container>
+        <Stack style={styles}>
+          <Skills />
+          <Work />
+          <Education />
+        </Stack>
       )}
     </Transition>
   );
